5df9cc4ceb4e011edec336ac113ceff8f2917c55
Ethan Guest <eguest01@gmail.com>
2021-05-05 09:01:09 -0500

    initial commit

---
+++ src/GraphicsPanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Forms;
+
+// Change the namespace to your project's namespace.
+namespace GOL
+{
+    internal class GraphicsPanel : Panel
+    {
+        // Default constructor
+        public GraphicsPanel()
+        {
+            // Turn on double buffering.
+            DoubleBuffered = true;
+
+            // Allow repainting when the window is resized.
+            SetStyle(ControlStyles.ResizeRedraw, true);
+        }
+
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            ResumeLayout(false);
+        }
+    }
+}
